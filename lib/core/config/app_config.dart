96b30dc7352c6a00da29fa7b8ff7e62c71c3ab37
rowan766 <row287630@gmail.com>
2025-12-25 10:27:55 +0800

    feat: 添加应用配置

---
+++ lib/core/config/app_config.dart
@@ -0,0 +1,6 @@
+class AppConfig {
+  static const String appName = '公路养护';
+  static const String apiBaseUrl = 'https://api.example.com';
+  static const int connectTimeout = 30000;
+  static const int receiveTimeout = 30000;
+}
