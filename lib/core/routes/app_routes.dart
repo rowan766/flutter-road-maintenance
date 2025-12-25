2111a96c7e0fbba1ac7068cf87e7e5d3da7adad3
rowan766 <row287630@gmail.com>
2025-12-25 10:28:12 +0800

    feat: 添加路由配置

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,8 @@
+class AppRoutes {
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String home = '/home';
+  static const String inspection = '/inspection';
+  static const String maintenance = '/maintenance';
+  static const String report = '/report';
+}
